225c23c084ddb8ba9f5de7bd493c172473715f81
fly-man- <fly-man-@109abde3-1f22-4c10-b5d7-b6b0135cd00c>
2008-10-29 00:18:44 +0000

    Updated register.php, new options from DataSnapShot that make it possible to delete a region from the hostsregister table (pt 2)
    git-svn-id: http://forge.opensimulator.org/svn/ossearch/trunk@95 109abde3-1f22-4c10-b5d7-b6b0135cd00c

--- webroot/register.php
+++ webroot/register.php
@@ -16,8 +16,9 @@ mysql_select_db ($DB_NAME);
 
 $hostname = $_GET['host'];
 $port = $_GET['port'];
+$service = $_GET['service'];
 
-if ($hostname != "" && $port != "")
+if ($hostname != "" && $port != "" && $service == "online")
 {
     // Check if there is already a database row for this host
     $checkhost = mysql_query("SELECT register from hostsregister where " .
@@ -47,5 +48,12 @@ if ($hostname != "" && $port != "")
 		$runupdate = mysql_query($register);
 	}
 }
+elseif ($hostname != "" && $port != "" && $service = "offline")
+{
+        $delete = "DELETE FROM hostsregister " .
+                "WHERE host = '" . mysql_escape_string($hostname) . "' AND " .
+                "port = '" . mysql_escape_string($port) . "'";
 
+        $rundelete = mysql_query($delete);
+}
 ?>
